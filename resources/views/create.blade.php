9d5fac2da731eca3fbfe52b606f9881dd87b79f0
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-11-12 15:25:52 -0300

    criando alert de erro

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,6 +7,15 @@
     <div class="col-sm-8 offset-sm-2">
         <h1 class="display-3">Adicionar produto</h1>
         <div>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div><br/>
+            @endif
             <form method="post" action="store">
                 @csrf
                 <div class="form-group">    
