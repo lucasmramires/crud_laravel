7d027442fd3d12db0755ccb70cee1a99dc8e7992
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-11-12 16:10:19 -0300

    criacao da view do transaction

---
+++ resources/views/transactions_index.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('main')
+<div class="row">
+<div class="col-sm-12">
+    <h1 class="display-3">Relatório</h1>
+    <a href="/" class="btn btn-primary">Voltar para home</a>
+    <h2>Movimentação do dia</h2>
+<div>
+</div>
+@endsection 
