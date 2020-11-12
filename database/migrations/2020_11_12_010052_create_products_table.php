678ab2ed21337eb4d4ca5f1ec58cc4a495f19b1f
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-11-12 00:16:52 -0300

    add sku na tabela

--- database/migrations/2020_11_12_010052_create_products_table.php
+++ database/migrations/2020_11_12_010052_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('product_id');
             $table->string('name', 64);
+            $table->string('sku', 30);
             $table->string('description', 256);
             $table->double('price');
             $table->integer('quantity_available');
