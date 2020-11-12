a3bfad181d310cf707028b4661ad7be04e5ae0e5
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-11-12 15:21:38 -0300

    criado view de criacao

---
+++ resources/views/create.blade.php
@@ -0,0 +1,11 @@
+<!-- extendendo app -->
+@extends('app')
+
+<!-- sessao principal -->
+@section('main')
+<div class="row">
+    <div class="col-sm-8 offset-sm-2">
+        <h1 class="display-3">Adicionar produto</h1>
+    </div>
+</div>
+@endsection
