5e63af18a65bad8ace6035d4b8c1fbc0f4d1bc76
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-11-12 16:12:18 -0300

    adicao botao de cancelamento

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -39,6 +39,7 @@
                 </div>
 
                 <button type="submit" class="btn btn-primary">Salvar</button>
+                <a href="/" class="btn btn-warning">Cancelar</a>
             </form>
         </div>
     </div>
